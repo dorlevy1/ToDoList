ec10a35d53434465b82bf2113cfc0639b1ea03de
dorlevy1 <49606382+dorlevy1@users.noreply.github.com>
2019-09-17 23:56:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,36 @@ var items = document.getElementById("items");
 var ul = document.querySelector("ul");
 var li = document.querySelector("li");
 var body = document.querySelector("body");
+var container1 = document.getElementById("container1");
+var deleteall = document.getElementById("deleteAll");
+var checkall = document.getElementById("checkALL");
 var counter = 0;
 var minusCount = 0;
 var shops = [];
+var storagee = [];
 
 function start() {
     if (shops.length == 0) {
         document.getElementById("amountMinus").innerHTML = "";
         document.getElementById("amount").innerHTML = "";
-        document.getElementById("container1").classList.replace("container11", "container1");
+        document.getElementById("container1").style.display = 'none';
 
     }
 }
+// var container = document.createElement("div");
+// container.className = "container";
+// body.appendChild(container);
+// container.appendChild(document.getElementById("whole"));
+// // container.appendChild(document.querySelector("hr"));
+// // container.appendChild(h1);
+// // container.appendChild(items);
+// // container.appendChild(button);
+// // container.appendChild(ul);
+// // container.appendChild(document.querySelector("div"));
+// // container.appendChild(document.querySelector("script"));
+// // container.appendChild(document.querySelector("hr"));
+// // container.appendChild(li);
+
 function itemLength() {
     return items.value.length;
 }
@@ -40,7 +58,8 @@ function createListElement() {
     ul.appendChild(button2);
     button2.setAttribute("id", "removeLi");
     shops.push(document.createTextNode(items.value));
-
+    deleteall.addEventListener("click", deleteAAll);
+    checkall.addEventListener("click", checkALL);
 
     function DeleteList() {
         if (li.classList.contains("done")) {
@@ -51,14 +70,42 @@ function createListElement() {
             document.getElementById("amountMinus").innerHTML = `<br>you deleted ${minusCount} lists`;
             amount();
             start();
+            // start(); // its if i wanna refresh the page .
         } else {
             alert("please asign the product that you wanna delete");
 
 
         }
     }
+
+    function checkALL(word) {
+        li.classList.toggle("done");
+        li.classList.toggle("span");
+        return storagee.push(word.value);
+    }
+
+    function deleteAAll() {
+        if (li.classList.contains("span")) {
+            li.remove();
+            button2.remove();
+            shops.shift(document.createTextNode(items.value));
+            document.getElementById("container1").style.display = 'none';
+        } else {
+            alert("you must check all");
+        }
+
+
+    }
     amount();
+
 }
+
+function ToggleDone() {
+    this.classList.toggle("done");
+    this.classList.toggle("span");
+}
+
+
 
 
 
@@ -72,17 +119,14 @@ function amount() {
     }
 }
 
-function ToggleDone() {
-    this.classList.toggle("done");
-    this.classList.toggle("span");
-}
+
 
 function AddListAfterClick() {
 
     if (!itemLength()) {
         return document.getElementById("err").innerHTML = "<br><br>please contain some product";
     } else if (itemLength() > 0) {
-        document.getElementById("container1").classList.replace("container1", "container11");
+        document.getElementById("container1").style.display = 'block';
         createListElement()
         document.getElementById("err").innerHTML = "";
 
@@ -98,14 +142,25 @@ function AddListAfterEnter(event) {
         document.getElementById("err").innerHTML = "";
     }
     if (event.keyCode === 13) {
-        document.getElementById("container1").classList.replace("container1", "container11");
+        document.getElementById("container1").style.display = 'block';
+
         createListElement();
+    }
+    if (checkListLength() >= 10) {
+        document.getElementById("deleteAll").style.display = 'block';
+        document.getElementById("checkALL").style.display = 'block';
+
     }
 }
 
+function bigScreen() {
+    return container1.style.transition = "all 2s ";
+
+}
+
+document.getElementById("deleteAll").style.display = 'none';
+document.getElementById("checkALL").style.display = 'none';
 document.getElementById("err").innerHTML = "<br><br>please contain some product";
-
-
-
 button.addEventListener("click", AddListAfterClick);
 items.addEventListener("keypress", AddListAfterEnter);
+container1.addEventListener("mouseover", bigScreen);
